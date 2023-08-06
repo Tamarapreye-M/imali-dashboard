72daa011ff1c9cc73ddc553dd49bf51d6cab9883
Obiofiong <johnobiofiong1@gmail.com>
2023-08-06 17:46:16 +0100

    card commit

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Image from "next/image";
 
 function Card({ image, title, value }) {
   return (
-    <div className="rounded-small shadow-md min-w-[100px] w-full p-8 bg-[#ffff]">
-      <Image src={image} alt={"card-img"} className="w-14 h-14" />
-
-      <p className=" my-2 mt-8">{title}</p>
-      <p className=" font-bold text-2xl mt-2">{value}</p>
+    <div className="rounded-small shadow-md h-[250px] min-w-[100px] p-12">
+      {/* <Image
+    src={image}
+     /> */}
+      helo
+      <p className="text-gray-400 my-2">{title}</p>
+      <p className="text-gray-400 font-extrabold text-5xl my-2">{value}</p>
     </div>
   );
 }
